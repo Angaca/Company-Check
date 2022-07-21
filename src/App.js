2b06a75aba337b393442d979126c9ac0cc51b735
Angaca <andreacatania91@gmail.com>
2022-07-21 13:41:58 +0100

    make axios request to GET companies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Map from "./Components/Map";
 
 function App() {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://run.mocky.io/v3/7cb595ed-2882-4dc7-8179-d38d0b9c9d13")
+      .then(({ data: companies }) => {
+        setCompanies(companies);
+      });
+  }, []);
+
+  console.log(companies);
+
   return (
     <div className="App">
       <Map />
